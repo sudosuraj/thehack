3b56f4c6685e4b81594c0cb1a0fd627a8d1a9464
sudosuraz <81553118+sudosuraz@users.noreply.github.com>
2022-01-23 15:27:13 +0530

    Done bash......

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-<style>:root{--primary-color:#367BF0;--body-color:#f9f9f9;--text-color:#636363;--text-color-dark:#242738;--white-color:#ffffff;--light-color:#f8f9fa;--font-family:Noto Sans}body.dark-theme{--body-color:black;--text-color:#e1e1e1;--text-color-dark:white;--white-color:#121212;--light-color:#1A1A1A}</style>
-<script>const $=document.querySelector.bind(document),$$=document.querySelectorAll.bind(document)</script>
 (function e(t, n, r) {
     function s(o, u) {
         if (!n[o]) {
